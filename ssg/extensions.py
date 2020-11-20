94a1e7a46284f157e2b74fda6d0731382b177f7a
Michael Thomson <github.com@m-thomson.net>
2020-11-20 12:08:42 +0000

    UNfair

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -9,7 +9,7 @@ def load_module(directory, name):
 
 def load_directory(directory):
     for path in directory.rglob("*.py"):
-        load_module(directory, path)
+        load_module(directory.as_posix(), path.stem)
 
 def load_bundled():
     directory = Path(__file__).parent / "extensions"
